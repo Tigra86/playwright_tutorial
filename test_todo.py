79117ac1986cacb2b80a97ab64b5a1a1108e63e7
Tigran <tigra.azaryan@me.com>
2025-02-11 14:09:46 -0800

    Update test_todo.py

--- test_todo.py
+++ test_todo.py
@@ -7,3 +7,11 @@ def test_add_todo(page):
     page.get_by_placeholder("What needs to be done?").fill("Создать первый сценарий playwright")
     page.get_by_placeholder("What needs to be done?").press("Enter")
     page.get_by_role("checkbox", name="Toggle Todo").check()
+
+
+def test_loc(page):
+    page.goto('https://zimaev.github.io/text_input/')
+    page.get_by_label("Email address").fill("qa@example.com")
+    page.get_by_title("username").fill("Anton")
+    page.get_by_placeholder('password').fill("secret")
+    page.get_by_role('checkbox').click()
