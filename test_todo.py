c19a098fb65e993ccf2440b86d553527e80bbc85
Tigran <tigra.azaryan@me.com>
2025-02-11 15:57:17 -0800

    Update test_todo.py

--- test_todo.py
+++ test_todo.py
@@ -1,17 +1,16 @@
 from playwright.sync_api import Playwright, sync_playwright, expect
 
 
-def test_add_todo(page):
-    page.goto("https://demo.playwright.dev/todomvc/#/")
-    page.get_by_placeholder("What needs to be done?").click()
-    page.get_by_placeholder("What needs to be done?").fill("Создать первый сценарий playwright")
-    page.get_by_placeholder("What needs to be done?").press("Enter")
-    page.get_by_role("checkbox", name="Toggle Todo").check()
-
-
-def test_loc(page):
-    page.goto('https://zimaev.github.io/text_input/')
-    page.get_by_label("Email address").fill("qa@example.com")
-    page.get_by_title("username").fill("Anton")
-    page.get_by_placeholder('password').fill("secret")
-    page.get_by_role('checkbox').click()
+def test_todo(page):
+    page.goto('https://demo.playwright.dev/todomvc/#/')
+    expect(page).to_have_url("https://demo.playwright.dev/todomvc/#/")
+    input_field = page.get_by_placeholder('What needs to be done?')
+    expect(input_field).to_be_empty()
+    input_field.fill("Закончить курс по playwright")
+    input_field.press('Enter')
+    input_field.fill("Добавить в резюме, что умею автоматизировать")
+    input_field.press('Enter')
+    todo_item = page.get_by_test_id('todo-item')
+    expect(todo_item).to_have_count(2)
+    todo_item.get_by_role('checkbox').nth(0).click()
+    expect(todo_item.nth(0)).to_have_class('completed')
